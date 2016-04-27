74a0076d94780760749eebf85c5a6e32d8fb9fc5
Zack Story <ztstory@gmail.com>
2016-04-26 21:23:44 -0700

    es5, no build

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { createTransform } from 'redux-persist'
-import LZ from 'lz-string'
-import stringify from 'json-stringify-safe'
+var reduxPersist = require('redux-persist')
+var createTransform = reduxPersist.createTransform
+var LZ = require('lz-string')
+var stringify = require('json-stringify-safe')
 
 function inbound (state, key) {
   if (typeof state !== 'string') state = stringify(state)
@@ -12,9 +13,14 @@ function outbound (state, key) {
     if (process.env.NODE_ENV !== 'production') console.error('redux-persist-transform-compress: expected outbound state to be a string')
     return state
   }
-  return LZ.decompress(state)
+  try {
+    var newState = JSON.parse(LZ.decompress(state))
+    return newState
+  } catch (err) {
+    return null
+  }
 }
 
-export default function (config) {
+module.exports = function (config) {
   return createTransform(inbound, outbound, config)
 }
