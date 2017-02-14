0e9a40fe263f731638b33767fc581190c88c8ec0
Karol Janyst <lapkom@gmail.com>
2017-02-14 10:25:57 +0900

    Rewrite in ES6, add typescript definitions

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { createTransform } from "redux-persist";
+import LZ from "lz-string";
+import stringify from "json-stringify-safe";
+import process from "process";
+
+export default function createTransformCompress(config) {
+  return createTransform(
+      (state) => LZ.compressToUTF16(stringify(state)),
+      (state) => {
+          if (typeof state !== "string") {
+              if (process.env.NODE_ENV !== "production") {
+                  console.error("redux-persist-transform-compress: expected outbound state to be a string")
+              }
+              return state
+          }
+
+          try {
+              return JSON.parse(LZ.decompressFromUTF16(state))
+          } catch (err) {
+              if (process.env.NODE_ENV !== "production") {
+                  console.error("redux-persist-transform-compress: error while decompressing state", err)
+              }
+              return null
+          }
+      },
+      config
+  )
+}
